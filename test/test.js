36f506b5d6d15bb616d34729aae91a1262de5628
Ivan Voischev <voischev.ivan@ya.ru>
2015-10-19 22:48:12 +0300

    add module wrapper

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var render = require('..');
+var render = require('../lib/posthtml-render.js');
 var describe = require('mocha').describe;
 var it = require('mocha').it;
 var expect = require('chai').expect;
